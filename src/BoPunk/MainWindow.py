b10a4ba85ef8de264121db22d55438e49a08f010
Jaremy Creechley <creechley@gmail.com>
2009-02-02 11:34:10 -0700

    cleaned up code a bit

--- src/BoPunk/MainWindow.py
+++ src/BoPunk/MainWindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'interface/BoPunkMainWindow.ui'
 #
-# Created: Thu Jan 29 12:26:58 2009
+# Created: Mon Feb  2 11:25:14 2009
 #      by: PyQt4 UI code generator 4.4.4
 #
 # WARNING! All changes made in this file will be lost!
